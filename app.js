984304b1c7dbbe5dd169f938de3b34d6a50fd707
Crypto Gnome <33667144+CryptoGnome@users.noreply.github.com>
2024-02-01 10:51:32 -0500

    Update to Forge Values

--- app.js
+++ app.js
@@ -1056,8 +1056,8 @@ async function upgradeItem(username, item_number, quantity) {
             item.level = 1;
         }
 
-        //to upgrade an item user must send 10% more flux than the salvage value
-        if (quantity < value * 1.098) {
+        //to upgrade an item user must send 5% salvage value * level in flux
+        if (quantity < value * 0.05 * item.level) {
             console.log('User: ' + username + ' did not send the correct amount of flux to upgrade item: ' + item_number);
             return false;
         }
